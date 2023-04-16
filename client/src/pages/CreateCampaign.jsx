cbc319cfbf7c3c0e7451730dd6d3ae82c505c710
Phakin Neamson <92076305+winzter@users.noreply.github.com>
2023-04-17 01:34:18 +0700

    123

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -10,7 +10,7 @@ import { checkIfImage } from '../utils';
 const CreateCampaign = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-  const { createProject } = useStateContext();
+  const { createCamapign } = useStateContext();
   const [form, setForm] = useState({
     name: '',
     title: '',
@@ -30,7 +30,7 @@ const CreateCampaign = () => {
     checkIfImage(form.image, async (exists) => {
       if(exists) {
         setIsLoading(true)
-        await createProject({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
+        await createCamapign({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
         setIsLoading(false);
         navigate('/');
       } else {
